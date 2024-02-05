ddf3d805e6380cdf6b8c86b69ff053d0e94cde17
HoadvRP <hoadv@reactplus.jp>
2024-02-05 14:57:23 +0700

    done octavian 3

---
+++ 8-Octavian3/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuOpen = document.querySelector(".menu-toggle");
+  const menu = document.querySelector(".menu");
+  if (!menuOpen || !menu) return;
+  menuOpen.addEventListener("click", function () {
+    menu.classList.add("is-active");
+  });
+  document.addEventListener("click", function (e) {
+    if (!menu.contains(e.target) && !e.target.matches(".menu-toggle")) {
+      menu.classList.remove("is-active");
+    }
+  });
+});
